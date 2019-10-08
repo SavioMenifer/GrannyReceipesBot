aac0ea03dc31e0885776d281f205095709b64128
saviomenifer <saviomenifer@gmail.com>
2019-10-08 11:29:48 +0200

    add streaming API

--- GrannyRecipesBot/GrannyRecipesBot.py
+++ GrannyRecipesBot/GrannyRecipesBot.py
@@ -22,13 +22,19 @@ api = twitter.Api(consumer_key=api_key,
 sp_api = sp.API(spoonacular_key)
 
 # Detect the food mentions in the tweet
-def detectFoodInTweet(tweet):
-    response = sp_api.detect_food_in_text(tweet)
+def detectFoodInTweet(tweet_text):
+    response = sp_api.detect_food_in_text(tweet_text)
     data = response.json()
     return data["annotations"]
 
 def getRecipeDetails(json_recipe):
-    return [json_recipe[0]['id'], json_recipe[0]['title'], json_recipe[0]['image']]
+    if len(json_recipe) > 0:
+        response = sp_api.get_recipe_information(json_recipe[0]['id'])
+        print(response.headers['X-API-Quota-Used'], "points used today.") #print spoonacular quota used.
+        data = response.json()
+        return [json_recipe[0]['id'], json_recipe[0]['title'], json_recipe[0]['image'], data['sourceUrl']]
+    else:
+        return None
 
 # Return a recipe depending on the first detected food item
 def returnRecipe(food):
@@ -43,11 +49,13 @@ def returnRecipe(food):
     print(ingredients_string)
 
     if len(dishes) > 0:
-        print("found a dish!", dishes[0]['annotation'])
+        print("Found a dish!", dishes[0]['annotation'])
         response = sp_api.search_recipes_complex(query = dishes[0]['annotation'], number = 1, includeIngredients = ingredients_string)
         search_result = response.json()
-        return getRecipeDetails(search_result['results'])
-    elif len(ingredients) > 0:
+        recipes = getRecipeDetails(search_result['results'])
+        if recipes:
+            return recipes
+    if len(ingredients) > 0:
         response = sp_api.search_recipes_by_ingredients(ingredients = ingredients_string, number = 1)
         search_result = response.json()
         return getRecipeDetails(search_result)
@@ -66,56 +74,58 @@ def returnRecipe(food):
 #make haste and start now!
 
 def grandmaSentences(number):
-
     sentences = [
-    ['header', 'Hello dear', 'Hello lovely' , 'Hello my dear,', 'Thanks for your message',],
+    ['header', 'Hello dear', 'Hello lovely' , 'Hello my dear', 'Thanks for your message'],
     ['randomReason',
-         'Hello?... My hearing aid is not working properly. ', 'Oops... deary me! I forgot my reading glasses.', 
-         'Yeah sorry... Didn\'t know you were talking to me. ', 'Back in my day, we did not use those ingredients. '],
+         'Hello?... My hearing aid is not working properly.', 'Oops... deary me! I forgot my reading glasses.', 
+         'Yeah sorry... Didn\'t know you were talking to me.', 'Back in my day, we did not use those ingredients.'],
     ['randomIngredients', 'Could you repeat those ingredients for me darling?', 'What ingredients did you mention dear?',
     'It would be marvellous if you can tell me those ingredients again, darling. Just one more time.'],    
     ['closure', 'Tell me if you liked it!', 'Enjoy cooking!', 'Granny out.', 'Have a nice meal!', 'Bon Appetite!',
      'Make haste and start now, so you can enjoy your marvellous meal sweetheart.'], 
-    ['foundrecipe', 'Granny recommends ', 'I would make ', 'You should try the ', 'You should definitely cook ', 'I wrote this one on paper, let me see. Ah '],
+    ['foundrecipe', 'Granny recommends ', 'I would make ', 'you should try the ', 'you should definitely cook ', 'I wrote this one on paper, let me see. Ah! '],
     ]
     return sentences[number][randrange(1, len(sentences[number]))]
 
-def createResponse(title):
+def createResponse(title, sourceUrl):
     response = ""
     header = grandmaSentences(0)
     foundRecipe = grandmaSentences(4)
     closure = grandmaSentences(3)
-    response = header + "\n" + foundRecipe + title + "\n" + closure
+    response = header + ", " + foundRecipe + title + ". " + closure + "\n" + sourceUrl
     return response
 
-def createRandomResponse(user):
+def createRandomResponse():
     response = ""
     randomIngredients = grandmaSentences(2)
     randomReason = grandmaSentences(1)
-    response = user + " " + randomReason + '\n' + randomIngredients
+    response = randomReason + " " + randomIngredients
     return response
 
 def tweetResponse(response, tweet_id):
     print("Posting reply...")
-    print("tweet id ", tweet_id)
+    print("tweet id", tweet_id)
     print(response)
     result = api.PostUpdate(response, in_reply_to_status_id=tweet_id, auto_populate_reply_metadata=True)
 
-# Get tweet by mentions
-mentions = api.GetMentions()
-print(mentions[0])
-createID = mentions[0].id
 
-#for mention in mentions:
-print("Mention text: ", mentions[0].text)
+print("Script started.")
 
-#play the demo, to be extended
-#for tweet in mentions:
-tweet = mentions[0]
-food_in_tweet = detectFoodInTweet(tweet)
-recipe = returnRecipe(food_in_tweet)
-print(recipe)
+mention_stream = api.GetStreamFilter(None, ['@BotGranny'])
 
+while True:
+    for tweet in mention_stream:
+        print("Received mention:", tweet['text'])
+        createID = tweet['id']
 
-tweetResponse(createResponse(recipe[1]), createID)
-#print(createRandomResponse())
+        food_in_tweet = detectFoodInTweet(tweet['text'])
+        recipe = returnRecipe(food_in_tweet)
+        if recipe:
+            print("Recipe found:", recipe)
+            response = createResponse(recipe[1], recipe[3])
+        else:
+            print("Recipe not found!")
+            response = createRandomResponse()
+
+        tweetResponse(response, createID)
+        print("Reply posted.")
